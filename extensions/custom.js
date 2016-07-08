d82570f6e9254e04c868bcdb3b5a884afef08fe8
Guilherme Beltramini <gcbeltramini@users.noreply.github.com>
2016-07-08 18:30:58 -0300

    Add instructions

--- extensions/custom.js
+++ extensions/custom.js
@@ -1,4 +1,5 @@
 /*
+
 INSTALLATION
 ============
 To install the extensions, copy the file custom.js to folder ~/.jupyter/custom
@@ -14,14 +15,23 @@ REFERENCES
  
  Some other cool IPython notebook extensions:
  - https://github.com/ipython-contrib/IPython-notebook-extensions
+
+
+To write custom javascript functions. Explore the object properties with:
+
+var cell = IPython.notebook.get_selected_cell();
+console.log(Object.keys(cell).sort());
+console.log(Object.getOwnPropertyNames(cell).sort());
+
+Or use the autocomplete in the code editor.
+
  */
 
 
 $([IPython.events]).on('app_initialized.NotebookApp', function(){
-	require(['/custom/toggle_prompt.js'])
-	require(['/custom/toggle_input_output.js'])
-	require(['/custom/TOC.js'])
-	require(['/custom/TOC_links.js'])
-    });
-
-console.log("Custom estensions loaded successfully!")
+    require(['/custom/toggle_prompt.js'])
+    require(['/custom/toggle_input_output.js'])
+    require(['/custom/auxiliary_TOC_fn.js'])
+    require(['/custom/TOC.js'])
+    require(['/custom/TOC_links.js'])
+});
